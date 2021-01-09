0430e9c66297d50e32641e808291cff1bad6b8fa
Alessio Margelli <alex9446@gmail.com>
2021-01-09 23:02:08 +0100

    📜 Added the plugin for broadcasting manifest info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 module.exports = {
   plugins: [
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Message List',
+        start_url: '/',
+        display: 'standalone',
+        icon: 'src/images/favicon.svg',
+        background_color: '#ffffff'
+      }
+    }
   ]
 }

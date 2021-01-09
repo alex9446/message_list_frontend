df38ab7d40f9fb30a873f84e2387d6413c33c779
Alessio Margelli <alex9446@gmail.com>
2021-01-10 00:48:57 +0100

    🧰 Added the plugin for offline support (service worker)

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,16 @@ module.exports = {
         start_url: '/',
         display: 'standalone',
         icon: 'src/images/favicon.svg',
+        cache_busting_mode: 'none',
         background_color: '#ffffff'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        workboxConfig: {
+          globPatterns: ['**/icons/*']
+        }
       }
     }
   ]
